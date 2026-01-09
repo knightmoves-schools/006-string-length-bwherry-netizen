03d654a36519fca3d80a1d397841e215ff6eb6f8
Brendon W. <bwherry@midland.k12.ia.us>
2026-01-09 18:31:03 +0000

    gg1

--- Speech.cs
+++ Speech.cs
@@ -1,10 +1,18 @@
-﻿namespace knightmoves;
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace knightmoves;
 public class Speech
 {   
     public const string Quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
+    public static int Length = Quote.Length; 
+    public char TwentySecondLetter = Quote[23];
+
     public string PrintLengthAndTwentySecondLetter(){
-        //Add your code here
-        return "";
+
+    string printLengthAndTwentySecondLetter = "length: {Length} and 22nd: {TwentySecondLetter}";
+
+
+        return printLengthAndTwentySecondLetter;
     }
 }
